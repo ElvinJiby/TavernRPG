a26c9d22610b89b4038a53d9fd77f1f3733390ff
ElvinJiby <elvinjiby04@gmail.com>
2024-09-20 13:41:07 +0100

    pygame setup

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("hi github!!!")
-print("1" + "2")
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('Tavern RPG')
+clock = pygame.time.Clock()
+running = True
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+dt = 0
+
+# Game Loop
+while running:
+    # Close window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Render game
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000 # Limits FPS to 60 & gets delta time
+
+pygame.quit()
